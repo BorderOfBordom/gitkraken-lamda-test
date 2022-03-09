005fc34e7f163da6f32fbf903b90d2a95d74b98d
Karan Chaudhari <karanmchaudhari@gmail.com>
2022-03-09 12:08:38 -0500

    Runner and interface files created

---
+++ PrintQuotient.java
@@ -0,0 +1,4 @@
+public interface PrintQuotient
+{
+	public void printQuotient(float numerator, float denominator);
+}
---
+++ Runner.java
@@ -0,0 +1,17 @@
+public class Runner implements PrintQuotient
+{
+	public void printQuotient(float numerator, float denominator)
+	{
+		//System.out.printf();
+	}
+
+	public Runner()
+	{
+
+	}
+
+	public static void main(String[] args)
+	{
+		Runner app = new Runner();
+	}
+}
