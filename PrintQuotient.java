21cefde5f5146a11bbaac99dd3cb47b3c53e0755
Karan Chaudhari <karanmchaudhari@gmail.com>
2022-03-11 11:43:22 -0500

    Functionality added to runner

--- PrintQuotient.java
+++
@@ -1,4 +0,0 @@
-public interface PrintQuotient
-{
-	public void printQuotient(float numerator, float denominator);
-}
--- Runner.java
+++ Runner.java
@@ -1,13 +1,14 @@
-public class Runner implements PrintQuotient
+public class Runner
 {
-	public void printQuotient(float numerator, float denominator)
+	interface PrintQuotient
 	{
-		//System.out.printf();
+		void printQuotient(float a, float b);
 	}
 
 	public Runner()
 	{
-
+		PrintQuotient func = (a,b) -> System.out.printf("The quotient of %.3f and %.3f is: %.3f\n", a, b, a/b);
+		func.printQuotient(1.0f, 2.0f);
 	}
 
 	public static void main(String[] args)
