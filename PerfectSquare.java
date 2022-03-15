b0fd6e1d01d834d91859d06c379863d54dfde680
Shaurya Agarwal <shaurya220@gmail.com>
2022-03-15 11:57:03 -0400

    Runner (PerfectSquare)

--- PerfectSquare.java
+++
@@ -1,4 +0,0 @@
-public interface PerfectSquare
-{
-	public boolean checkPerfect(int num);
-}
--- Runner.java
+++ Runner.java
@@ -1,18 +1,24 @@
-public class Runner implements PerfectSquare
+public class Runner
 {
 	public Runner()
 	{
+		PerfectSquare cP = (int num) ->
+		{
+			int x = (int)(Math.sqrt(num));
+			return (x*x == num);
+		};
 
+		System.out.println("Perfect Square (81): " + cP.checkPerfect(81));
+		System.out.println("Perfect Square (50): " + cP.checkPerfect(50));
 	}
 
-	public boolean checkPerfect(int num)
+	interface PerfectSquare
 	{
-		int x = (int)(Math.sqrt(num));
-		return (x*x == num);
+		boolean checkPerfect(int num);
 	}
 
 	public static void main(String[]args)
 	{
-
+		Runner runner = new Runner();
 	}
 }
